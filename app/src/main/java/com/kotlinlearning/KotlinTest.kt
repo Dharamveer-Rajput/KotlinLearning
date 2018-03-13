3a72916a2eed1c057ef179964ae31fc6551fd41b
dharamveer <dharamveer.smartitventures@gmail.com>
2018-03-13 18:18:17 +0530

    in

---
+++ app/src/main/java/com/kotlinlearning/KotlinTest.kt
@@ -0,0 +1,97 @@
+package com.kotlinlearning
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.kotlin_test_layout.*
+
+
+class KotlinTest : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.kotlin_test_layout)
+
+
+        //variables
+
+
+        //Integers
+
+        var x = 5
+        val y = 4
+
+        x = 6
+
+        println(x * y)
+
+        var age = 20
+
+        val result = age * 5 / 4
+
+        println(result)
+
+
+        //Only interger
+
+        val myInteger : Int = 5
+        val double : Double = 5.0
+
+
+        //Double & Float
+
+        val pi = 3.14
+
+        val myAge = 26.0
+
+        val myResult = myAge * 5/4
+
+        println(myResult)
+
+
+        //String
+
+        val  names = "dharam"
+        val  surname = "rajput"
+
+        val fullname  = names+ " " +surname
+        println(fullname)
+
+
+        val myName : String  = "Dharam"
+        //boolean
+
+        var isAlive : Boolean = true
+
+        isAlive = false
+
+
+
+        //Arrays
+
+        val myArray = arrayOfNulls<String>(4)
+
+        myArray[0] = "Dharam"
+        myArray[1] = "Rajput"
+        myArray[2] = "Kirk"
+        myArray[3] = "Rob"
+
+        println(myArray[2])
+
+
+
+        val myNumberArray = intArrayOf(10,20,30,40,50,60)
+        println(myNumberArray.size)
+        myNumberArray.set(2,25)
+        println(myNumberArray[2])
+
+
+
+
+    }
+
+
+
+}
