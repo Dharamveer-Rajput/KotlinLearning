8a6c6c63b4dbf2e264329ed69e26b0902eb9f062
dharamveer <dharamveer.smartitventures@gmail.com>
2018-03-14 15:37:23 +0530

    in

---
+++ app/src/main/java/com/kotlinlearning/layout_learning/LayoutLearning.kt
@@ -0,0 +1,15 @@
+package com.kotlinlearning.layout_learning
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlinlearning.R
+
+class LayoutLearning : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_learning)
+
+
+    }
+}
